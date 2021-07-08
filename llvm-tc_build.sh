02e57cd936b386e1a855614576430d3804e231d5
mose-to <84067577+mose-to@users.noreply.github.com>
2021-07-08 10:06:19 +0700

    Update llvm-tc_build.sh

--- llvm-tc_build.sh
+++ llvm-tc_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Use xRageTC build script as LLVM Build Script.
-git clone https://github.com/kdrag0n/proton-clang-build $(pwd)/proton-clang-build -b master
+git clone https://github.com/mose-to/proton-clang-build $(pwd)/proton-clang-build -b master
 cd $(pwd)/proton-clang-build
 bash build-toolchain.sh
